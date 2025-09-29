e49e98ed686eddecbdad79eafc32fa5a18656c73
tushar <tusharlevesl@icloud.com>
2025-09-28 22:35:10 -0400

    Roboshop Project all files

---
+++ catalogue.sh
@@ -0,0 +1,26 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp Catalogue_mongo_schema.repo /etc/yum.repos.d/mongo.repo
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload # change in service files
+systemctl enable catalogue
+systemctl start catalogue
+
+cp Catalogue_mongo_schema.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y
+
+mongosh --host mongobd-dev.tusharbytes.com </app/db/master-data.js
---
+++ frontend.sh
@@ -0,0 +1,14 @@
+
+#Disable nginx all, enable version 24, installing
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+cp nginx.conf /etc/nginx/nginx.conf
+
+rm -rf /usr/share/nginx/html/* #removing defAULT HTML CONTENT from NginX
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip # using -o means you are telling yoiur linux to save this file  to this specific name and this directory
+cd /usr/share/nginx/html  # you'd like to change dir first to unzip the file
+unzip /tmp/frontend.zip
+
+systemctl restart nginx
