d74e3fd8a0cd1a28e9322e84b0db3b88185552f0
tushar <tusharlevesl@icloud.com>
2025-09-30 15:07:25 -0400

    ansible

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,11 @@
+if [ -z "$1" ]; then
+  echo Password is missing
+  exit
+fi
+
+
 dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+
+mysql_secure_installation --set-root-pass $1 #RoboShop@1
