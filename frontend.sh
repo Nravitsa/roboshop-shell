f97740e9cff7c58df38b125550a803a9dcedfa32
tushar <tusharlevesl@icloud.com>
2025-10-07 16:28:09 -0400

    .

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,9 @@ dnf install nginx -y
 
 cp nginx.conf /etc/nginx/nginx.conf
 
+systemctl enable nginx
+systemctl start nginx
+
 rm -rf /usr/share/nginx/html/* #removing defAULT HTML CONTENT from NginX
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip # using -o means you are telling yoiur linux to save this file  to this specific name and this directory
 cd /usr/share/nginx/html  # you'd like to change dir first to unzip the file
